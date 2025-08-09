01d720c008b01f0bb2e0761cb57467250d45f017
Karan <karanpraja902@gmail.com>
2025-08-09 18:17:13 +0530

    feat: add @react-oauth/google and redux dependencies; update layout in Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -27,7 +27,7 @@ export default function Home() {
   return (
     <div className="flex h-screen">
       <ChatSidebar />
-      <div className="flex flex-col mt-60 mx-auto">
+      <div className="flex flex-col mt-60 mb-10  mx-auto">
         <div className="flex flex-col items-center gap-2 md:ml-40">
           <div className="flex items-center gap-4 justify-center">
             <div className="h-16 w-16">
@@ -39,11 +39,9 @@ export default function Home() {
             </div>
             <h2 className="text-2xl font-bold ">Hi, I'm DeepThink.</h2>
           </div>
-          <p className="text-center text-muted-foreground">
-            How can help you today
-          </p>
+         
         </div>
-        <div className="fixed left-0 top-30 right-0 bottom-0 mx-auto flex px-4 justify-center items-center">
+        <div className="fixed left-0 top-30 right-0 bottom-0 mx-auto flex px-4 justify-center items-center">{/**?ISLOADING */}
           <ChatInput onSubmit={handleSendMesaage} isLoading={isLoading} />
         </div>
       </div>
